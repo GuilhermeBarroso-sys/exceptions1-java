ef8f3a3336d7bcdae1596f3b8d36b6d7e4b26108
GolfinhoNinja <golfinhoninja@hotmail.com>
2020-08-18 14:54:15 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,25 +4,21 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
+import model.exception.DomainException;
 import model.entities.Reservation;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("Check-in date (dd/mm/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/mm/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		else {
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println(reservation);
 			System.out.println();
@@ -31,18 +27,19 @@ public class Program {
 			System.out.print("Check-out date (dd/mm/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			reservation.updateDates(checkIn, checkOut);
-			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println(error);
-			}
-			
-			else {
-				System.out.println(reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println(reservation);
+		}
+		catch(ParseException e) {
+			System.out.println("Invalid Date format");
+		}
+		catch(DomainException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		
 		
 	}
 
-}
+
 }
